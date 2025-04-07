6ea107f58895e5aff6953010f4f9e9953831cf1b
Hasaan Developer <muhammedhasaan09@gmail.com>
2025-04-08 00:40:33 +0500

    added reel popuo to hero section home page

--- src/pages/Contactus/ContactUs.jsx
+++ src/pages/Contactus/ContactUs.jsx
@@ -89,7 +89,7 @@ export default function Contactus() {
           <div className="relative rounded-lg overflow-hidden w-full">
             <iframe
               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3151.8354345097897!2d144.95373631531672!3d-37.8162797797517!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6ad642af0f11fd81%3A0x5045675218ce6e0!2sMelbourne%20VIC%2C%20Australia!5e0!3m2!1sen!2s!4v1633071291021!5m2!1sen!2s"
-              className="w-full h-64 sm:h-80 rounded-lg border"
+              className="w-full h-64 sm:h-80 rounded-lg"
               loading="lazy"
             ></iframe>
              <div className="mt-8">
